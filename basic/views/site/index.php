19a4a46a691ae5498d6cd73f52d6d002f32d3770
bulfinkl <01011988dima@gmail.com>
2017-09-17 23:18:33 +0300

    First Commit

---
+++ basic/views/site/index.php
@@ -0,0 +1,49 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Новости';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Новости в России</h1>
+
+        <p class="lead">Мы собираем информацию по кусочкам, создавая целустную картину мира</p>
+
+        <!--<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Путин уходит в отставку</h2>
+
+                <p>Сегодня стало известно, что Владимир Владимирович Путин уходит в отставку в связи с тем, что украл у 
+				государства 200 млрд. долларов США. Дональд Трам отреагировал следующим образом: "Путин великий человек, но вор".
+				</p>
+
+                <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>-->
+            </div>
+            <div class="col-lg-4">
+                <h2>Панда родила в автобусе</h2>
+
+                <p>В 9:00 утра по московскому времени в автобусе следующим из Чебоксар в Москву панда родила 4 малышей. Владельцы
+				этого животного везли ее в московский зоопарк, чтобы оставить ее там жить на ПМЖ. Папа панда остался дома, а вот
+				маму панду решили отдать профессионалам.</p>
+
+               <!-- <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>-->
+            </div>
+            <div class="col-lg-4">
+                <h2>Ледник начал движение</h2>
+
+                <p>Весь Северный Полюс начал движение к Белому морю. В диаметре ледник составляет 2 тыс. километров и вся эта ледяная
+				масса ползет в сторону России. Жители севера в панике, власти начали эвакуацию всего севера Российской Федерации.</p>
+
+                <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>-->
+            </div>
+        </div>
+
+    </div>
+</div>
